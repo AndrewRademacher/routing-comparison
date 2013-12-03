7fe9930bec990f036a1ac8230b40524d6f757b04
Andrew Rademacher <andrewrademacher@gmail.com>
2013-12-03 11:41:39 -0600

    Added search route to happstack.

--- happstack-test/src/Main.hs
+++ happstack-test/src/Main.hs
@@ -9,6 +9,7 @@ main = simpleHTTP nullConf $ msum
         [ dir "time" $ getTime
         , dir "add" $ path $ \n1 -> path $ \n2 -> getAddNum n1 n2
         , dir "add" $ path $ \w1 -> path $ \w2 -> getAddText w1 w2
+        , dir "search" $ getSearch
         , getHome
         ]
 
@@ -25,3 +26,14 @@ getAddNum n1 n2 = ok (toResponse (show (n1 + n2)))
 
 getAddText :: String -> String -> ServerPartT IO Response
 getAddText w1 w2 = ok (toResponse (w1 ++ w2))
+
+getSearch :: ServerPartT IO Response
+getSearch = do
+        contact <- queryString $ look "contactId"
+        company <- queryString $ look "companyId"
+        ok (toResponse (
+              "<ul>"
+           ++ "<li>Contact ID: " ++ contact ++ "</li>"
+           ++ "<li>Company ID: " ++ company ++ "</li>"
+           ++ "</ul>"
+           ))
